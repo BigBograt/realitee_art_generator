229136083952d4477f0f5434dc15e4def1a99f45
BigBograt <dan.murat@gmail.com>
2022-06-27 20:26:35 +0100

    counters, NONE Removal

--- main.py
+++ main.py
@@ -3,17 +3,9 @@ import os
 import json
 import random
 
-# dir_list = sorted(os.listdir(sticker_path))
-#
-# print("Files and directories in '", sticker_path, "' :")
-#
-# # prints all files
-# print(primary_path)
-
 
 def start_generation():
 
-    # Define image paths for each trait. Folder names do not represent meta values. filenames do.
     aura_path = "images/aura/"
     primary_path = "images/primary/"
     secondary_path = "images/secondary/"
@@ -24,22 +16,21 @@ def start_generation():
     trait2_path = "images/trait2/"
     trait3_path = "images/trait3/"
 
-    #Define ride type as it will be shown in the metadata
     ride_type = "Camper"
 
     meta = []
     counter = 0
     x = 1
 
-    while x <= 20:
-        aura = random.choice(sorted(os.listdir(aura_path)))
-        primary = random.choices(sorted(os.listdir(primary_path)), weights=(30, 3, 2, 15, 3, 2, 8, 20, 10, 5, 2, 4))[0]
+    while x <= 10:
+        aura = random.choices(sorted(os.listdir(aura_path)), weights=(1, 500))[0]
+        primary = random.choices(sorted(os.listdir(primary_path)), weights=(7, 5, 6, 6, 1, 4, 5, 2, 1, 1, 1, 1, 6, 7, 2, 1, 6, 6, 1, 7, 7, 3, 1, 1, 7, 3))[0]
         secondary = random.choice(sorted(os.listdir(secondary_path)))
         wheels = random.choices(sorted(os.listdir(wheels_path)), weights=(8, 90, 2))[0]
-        artwork = random.choices(sorted(os.listdir(artwork_path)), weights=(5, 25, 25, 5, 20, 20))[0]
+        artwork = random.choice(sorted(os.listdir(artwork_path)))
         lights = random.choices(sorted(os.listdir(lights_path)), weights=(80, 20))[0]
-        trait1 = random.choices(sorted(os.listdir(trait1_path)), weights=(5, 5, 10, 50, 10, 20))[0]
-        trait2 = random.choice(sorted(os.listdir(trait2_path)))
+        trait1 = random.choices(sorted(os.listdir(trait1_path)), weights=(6, 4, 2, 80, 6, 2))[0]
+        trait2 = random.choices(sorted(os.listdir(trait2_path)), weights=(1, 1, 1, 1, 96))[0]
         trait3 = random.choice(sorted(os.listdir(trait3_path)))
 
         temp_item = {
@@ -94,46 +85,58 @@ def start_generation():
             'attributes': [
                 {
                     'trait_type': 'Ride Type',
-                    'value': ride_type
+                    'value': ride_type.split('.png')[0]
                 },
                 {
-                    'trait_type': 'Aura',
-                    'value': aura
-                },
-                {
-                    'trait_type': 'Primary Monkey color',
-                    'value': primary
-                },
-                {
-                    'trait_type': 'Secondary color',
-                    'value': secondary
+                    'trait_type': 'Primary color',
+                    'value': primary.split('.png')[0]
                 },
                 {
                     'trait_type': 'Wheels',
-                    'value': wheels
-                },
-                {
-                    'trait_type': 'Artwork',
-                    'value': artwork
+                    'value': wheels.split('.png')[0]
                 },
                 {
                     'trait_type': 'Lights',
-                    'value': lights
-                },
-                {
-                    'trait_type': 'Feature One',
-                    'value': trait1
-                },
-                {
-                    'trait_type': 'Feature two',
-                    'value': trait2
-                },
-                {
-                    'trait_type': 'Feature Three',
-                    'value': trait3
+                    'value': lights.split('.png')[0]
                 }
             ]
         }
+
+        if aura.split('.png')[0].upper() != "NONE":
+            output_meta['attributes'].append({
+                    'trait_type': 'Aura',
+                    'value': aura.split('.png')[0]
+                })
+
+        if artwork.split('.png')[0].upper() != "NONE":
+            output_meta['attributes'].append({
+                    'trait_type': 'Artwork',
+                    'value': artwork.split('.png')[0]
+                })
+
+        if secondary.split('.png')[0].upper() != "NONE":
+            output_meta['attributes'].append({
+                    'trait_type': 'Secondary color',
+                    'value': secondary.split('.png')[0]
+                })
+
+        if trait1.split('.png')[0].upper() != "NONE":
+            output_meta['attributes'].append({
+                    'trait_type': 'Feature One',
+                    'value': trait1.split('.png')[0]
+                })
+
+        if trait2.split('.png')[0].upper() != "NONE":
+            output_meta['attributes'].append({
+                    'trait_type': 'Feature One',
+                    'value': trait2.split('.png')[0]
+                })
+
+        if trait3.split('.png')[0].upper() != "NONE":
+            output_meta['attributes'].append({
+                    'trait_type': 'Feature One',
+                    'value': trait3.split('.png')[0]
+                })
 
         if temp_item in meta:
             print("Duplicate LFG again")
@@ -143,7 +146,7 @@ def start_generation():
             with open("final/" + str(x) + '.json', 'w') as f:
                 json.dump(output_meta, f, sort_keys=True, indent=4, ensure_ascii=False)
 
-            # DO NOT AMEND BASE IMAGE this is a blank image
+            # DO NOT AMEND BASE IMAGE
             base_image = Image.open("images/base.png")
 
             aura_img = Image.open(aura_path + aura)
@@ -168,42 +171,38 @@ def start_generation():
 
             base_image.save('final/' + str(x) + '.png')
 
-            # Static Image used to show wihout APE ######
-            # This section OPENS all images ready to be layered
-            base_image = Image.open("images/base.png")
-
-            aura_img = Image.open(aura_path + aura)
-            primary_img = Image.open(primary_path + primary)
-            secondary_img = Image.open(secondary_path + secondary)
-            wheels_img = Image.open(wheels_path + wheels)
-            artwork_img = Image.open(artwork_path + artwork)
-            lights_img = Image.open(lights_path + lights)
-            trait1_img = Image.open(trait1_path + trait1)
-            trait2_img = Image.open(trait2_path + trait2)
-            trait3_img = Image.open(trait3_path + trait3)
-
-            # ---- This image on top of full ride to fill the gap
-            back_piece = Image.open("images/backpiece.png")
-
-            # This section LAYERS AND SAVES all imagery.
-            base_image.paste(aura_img, (0, 0), mask=aura_img)
-            base_image.paste(primary_img, (0, 0), mask=primary_img)
-            base_image.paste(secondary_img, (0, 0), mask=secondary_img)
-            base_image.paste(wheels_img, (0, 0), mask=wheels_img)
-            base_image.paste(artwork_img, (0, 0), mask=artwork_img)
-            base_image.paste(lights_img, (0, 0), mask=lights_img)
-            base_image.paste(trait1_img, (0, 0), mask=trait1_img)
-            base_image.paste(trait2_img, (0, 0), mask=trait2_img)
-            base_image.paste(trait3_img, (0, 0), mask=trait3_img)
-
-            # --- This image on top of full ride to fill the gap
-            base_image.paste(back_piece, (0, 0), mask=back_piece)
-
-            base_image.save('final/' + str(x) + '_main.png')
-
             # Do not alter below
+            print(x)
             counter += 1
             x += 1
+
+    print('Primary: \n')
+    print('Aquamarine: ' + str(str(meta).count('Aquamarine')))
+    print('Army Green: ' + str(str(meta).count('Army Green')))
+    print('Black: ' + str(str(meta).count('Black')))
+    print('Blue: ' + str(str(meta).count('Blue')))
+    print('Camo: ' + str(str(meta).count('Camo')))
+    print('Cheetah: ' + str(str(meta).count('Cheetah')))
+    print('Cream: ' + str(str(meta).count('Cream')))
+    print('DMT: ' + str(str(meta).count('DMT')))
+    print('Dots: ' + str(str(meta).count('Dots')))
+    print('Galaxy: ' + str(str(meta).count('Galaxy')))
+    print('Giraffe: ' + str(str(meta).count('Giraffe')))
+    print('Gold: ' + str(str(meta).count('Gold')))
+    print('Gray: ' + str(str(meta).count('Gray')))
+    print('Green: ' + str(str(meta).count('Green')))
+    print('Leather: ' + str(str(meta).count('Leather')))
+    print('Noise: ' + str(str(meta).count('Noise')))
+    print('Orange: ' + str(str(meta).count('Orange')))
+    print('Pink: ' + str(str(meta).count('Pink')))
+    print('Pilkadot: ' + str(str(meta).count('Pilkadot')))
+    print('Purple: ' + str(str(meta).count('Purple')))
+    print('Red: ' + str(str(meta).count('Red')))
+    print('Rust: ' + str(str(meta).count('Rust')))
+    print('Trippy: ' + str(str(meta).count('Trippy')))
+    print('Weed: ' + str(str(meta).count('Weed')))
+    print('Yellow: ' + str(str(meta).count('Yellow')))
+    print('Zombie: ' + str(str(meta).count('Zombie')))
 
 
 start_generation()
--- test.py
+++ test.py
@@ -1,1 +1,155 @@
-# file not in use
+from PIL import Image
+import shutil
+import os
+import json
+import random
+
+# dir_list = sorted(os.listdir(sticker_path))
+#
+
+# print("Files and directories in '", sticker_path, "' :")
+#
+# # prints all files
+# print(primary_path)
+
+
+def start_generation():
+
+    primary_path = "images/primary/"
+    secondary_path = "images/secondary/"
+
+    sticker_path = "images/stickers/"
+    trait_path = "images/traits/"
+
+    meta = []
+    counter = 0
+    x = 1
+
+    while x <= 20:
+
+        primary = random.choices(os.listdir(primary_path), weights=(10, 20, 30, 40, 50), k=5)
+        a = str(meta).count(primary)
+        primary_value = primary.split('#')
+
+        if str(a) == primary_value[0]:
+            shutil.move(primary_path + primary, "used/" + primary_path + primary)
+            primary = random.choice(os.listdir(primary_path))
+
+        secondary = random.choice(os.listdir(secondary_path))
+        a = str(meta).count(secondary)
+        secondary_value = secondary.split('#')
+        if str(a) == secondary_value[0]:
+            shutil.move(secondary_path + secondary, "used/" + secondary_path + secondary)
+            secondary = random.choice(os.listdir(secondary_path))
+
+        sticker = random.choice(os.listdir(sticker_path))
+        b = str(meta).count(sticker)
+        sticker_value = sticker.split('#')
+        if str(b) == sticker_value[0]:
+            shutil.move(sticker_path + sticker, "used/" + sticker_path + sticker)
+            sticker = random.choice(os.listdir(sticker_path))
+
+        trait = random.choice(os.listdir(trait_path))
+        c = str(meta).count(trait)
+        trait_value = trait.split('#')
+        if str(c) == trait_value[0]:
+            shutil.move(trait_path + trait, "used/" + trait_path + trait)
+            trait = random.choice(os.listdir(trait_path))
+
+        temp_item = {
+            'attributes': [
+                {
+                    'trait_type': 'Primary color',
+                    'value': primary_value[1]
+                },
+                {
+                    'trait_type': 'Secondary color',
+                    'value': secondary_value[1]
+                },
+                {
+                    'trait_type': 'Sticker',
+                    'value': sticker_value[1]
+                },
+                {
+                    'trait_type': 'trait1',
+                    'value': trait_value[1]
+                }
+            ]
+        }
+
+        output_meta = {
+            'name': 'AR #' + str(x),
+            'description': 'AR LFG',
+            'image': 'https://storage.googleapis.com/aperides/rides/images/' + str(x) + '.png',
+            'attributes': [
+                {
+                    'trait_type': 'Primary Monkey color',
+                    'value': primary_value[1]
+                },
+                {
+                    'trait_type': 'Secondary color',
+                    'value': secondary_value[1]
+                },
+                {
+                    'trait_type': 'Sticker',
+                    'value': sticker_value[1]
+                },
+                {
+                    'trait_type': 'trait1',
+                    'value': trait_value[1]
+                }
+            ]
+        }
+
+        if temp_item in meta:
+            print("Duplicate LFG again")
+        else:
+            meta.append(temp_item)
+
+            with open("final/" + str(x) + '.json', 'w') as f:
+                json.dump(output_meta, f, sort_keys=True, indent=4, ensure_ascii=False)
+
+            # print(meta)
+
+            img1 = Image.open(primary_path + primary)
+            img2 = Image.open(secondary_path + secondary)
+            img3 = Image.open("images/traits/" + trait)
+            img4 = Image.open("images/stickers/" + sticker)
+
+            img1.paste(img2, (0, 0), mask=img2)
+            img1.paste(img3, (0, 0), mask=img3)
+            img1.paste(img4, (0, 0), mask=img4)
+
+            img1.save('final/' + str(x) + '.png')
+
+            # Image Back ######
+            img1 = Image.open("images/back/primary/" + primary)
+            img2 = Image.open(secondary_path + secondary)
+            img3 = Image.open("images/traits/" + trait)
+            img4 = Image.open("images/stickers/" + sticker)
+
+            img1.paste(img2, (0, 0), mask=img2)
+            img1.paste(img3, (0, 0), mask=img3)
+            img1.paste(img4, (0, 0), mask=img4)
+
+            img1.save('final/' + str(x+30) + '_main.png')
+            # Image Back ######
+
+            # Image No AOR ######
+            img1 = Image.open("images/back/primary/" + primary)
+            img2 = Image.open(secondary_path + secondary)
+            img3 = Image.open("images/traits/" + trait)
+            img4 = Image.open("images/stickers/" + sticker)
+
+            img1.paste(img2, (0, 0), mask=img2)
+            img1.paste(img3, (0, 0), mask=img3)
+            img1.paste(img4, (0, 0), mask=img4)
+
+            img1.save('final/' + str(x) + '_main.png')
+            # Image No AOR ######
+
+            counter += 1
+            x += 1
+
+
+start_generation()
